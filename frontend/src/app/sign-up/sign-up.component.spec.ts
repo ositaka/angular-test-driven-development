05f318c7d623378c41c86230bd80f4c8a73c30da
Nuno Marques <ositaka@gmail.com>
2024-04-13 22:28:55 +0200

    First tests with Jasmine and Jest

---
+++ frontend/src/app/sign-up/sign-up.component.spec.ts
@@ -0,0 +1,104 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SignUpComponent } from './sign-up.component';
+
+describe('SignUpComponent', () => {
+  let component: SignUpComponent;
+  let fixture: ComponentFixture<SignUpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SignUpComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SignUpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  describe('Layout', () => {
+    it('has Sign Up header', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const h1 = signUp.querySelector('h1');
+      expect(h1?.textContent).toBe('Sign Up');
+    });
+
+    it('has username input', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const label = signUp.querySelector('label[for="username"]');
+      const input = signUp.querySelector('input[id="username"]');
+      expect(input).toBeTruthy();
+      expect(label).toBeTruthy();
+      expect(label?.textContent).toContain('Username');
+    });
+
+    it('has email input', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const label = signUp.querySelector('label[for="email"]');
+      const input = signUp.querySelector('input[id="email"]');
+      expect(input).toBeTruthy();
+      expect(label).toBeTruthy();
+      expect(label?.textContent).toContain('E-mail');
+    });
+
+    it('has password input', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const label = signUp.querySelector('label[for="password"]');
+      const input = signUp.querySelector('input[id="password"]');
+      expect(input).toBeTruthy();
+      expect(label).toBeTruthy();
+      expect(label?.textContent).toContain('Password');
+    });
+
+    it('has password type for password input', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const input = signUp.querySelector(
+        'input[id="password"]'
+      ) as HTMLInputElement;
+      expect(input?.type).toBe('password');
+    });
+
+    it('has password repeat input', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const label = signUp.querySelector('label[for="passwordRepeat"]');
+      const input = signUp.querySelector('input[id="passwordRepeat"]');
+      expect(input).toBeTruthy();
+      expect(label).toBeTruthy();
+      expect(label?.textContent).toContain('Password');
+    });
+
+    it('has password repeat type for password input', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const input = signUp.querySelector(
+        'input[id="passwordRepeat"]'
+      ) as HTMLInputElement;
+      expect(input?.type).toBe('password');
+    });
+
+    it('has Sign Up button', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const button = signUp.querySelector('button');
+      expect(button?.textContent).toBe('Sign Up');
+    });
+
+    it('disables the button initially', () => {
+      const signUp = fixture.nativeElement as HTMLElement;
+      const button = signUp.querySelector('button');
+      expect(button?.disabled).toBeTruthy();
+    });
+  });
+});
+
+// ------------------------------------------------------------------
+// Example of a test that uses the same test as the Jest test,
+// but instead of using expect(header).toBeInTheDocument();,
+// it uses expect(header).toBeTruthy();.
+// ------------------------------------------------------------------
+// import { render, screen } from '@testing-library/angular';
+// import { SignUpComponent } from './sign-up.component';
+
+// it('has Sign Up header', async () => {
+//   await render(SignUpComponent);
+//   const header = screen.getByRole('heading', { name: 'Sign Up' });
+//   expect(header).toBeTruthy();
+// });

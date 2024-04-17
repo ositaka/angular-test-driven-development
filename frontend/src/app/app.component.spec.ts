ff8e8319641e01fe9a5327fd59403b1fd9c06df1
Nuno Marques <ositaka@gmail.com>
2024-04-17 06:38:28 +0200

    Added more tests for routing (passing).

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { Router, provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
+import { UserService } from './core/user.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -10,7 +12,7 @@ describe('AppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule],
       providers: [
         provideRouter(routes),
         withHashLocation(),
@@ -28,7 +30,25 @@ describe('AppComponent', () => {
   describe('Routing', () => {
     it('displays homepage at /', async () => {
       await router.navigate(['/']);
-      const page = fixture.nativeElement.querySelector('[data-testid="homepage"]');
+      const page = fixture.nativeElement.querySelector('[data-testid="home-page"]');
+      expect(page).toBeTruthy();
+    });
+
+    it('displays sign up page at /signup', async () => {
+      await router.navigate(['/signup']);
+      const page = fixture.nativeElement.querySelector('[data-testid="signup-page"]');
+      expect(page).toBeTruthy();
+    });
+
+    it('displays login page at /login', async () => {
+      await router.navigate(['/login']);
+      const page = fixture.nativeElement.querySelector('[data-testid="login-page"]');
+      expect(page).toBeTruthy();
+    });
+
+    it('displays user page at /user', async () => {
+      await router.navigate(['/user']);
+      const page = fixture.nativeElement.querySelector('[data-testid="user-page"]');
       expect(page).toBeTruthy();
     });
   });
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +13,19 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SignUpComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: ":id",
+        component: UserDetailComponent
+      }
+    ]
   },
 ];

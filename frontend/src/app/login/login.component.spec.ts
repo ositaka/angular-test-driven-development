f810826fe9f61a2129b986da67936da0acf9610a
Nuno Marques <ositaka@gmail.com>
2024-04-22 06:54:18 +0200

    Added Login Page tests

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,16 +10,145 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent, HttpClientTestingModule, FormsModule]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Layout', () => {
+    it('has Login header', () => {
+      const loginPage = fixture.nativeElement as HTMLElement;
+      const h1 = loginPage.querySelector('h1');
+      expect(h1?.textContent).toBe('Login');
+    });
+
+    it('has email input', () => {
+      const loginPage = fixture.nativeElement as HTMLElement;
+      const label = loginPage.querySelector('label[for="email"]');
+      const input = loginPage.querySelector('input[id="email"]');
+      expect(input).toBeTruthy();
+      expect(label).toBeTruthy();
+      expect(label?.textContent).toContain('E-mail');
+    });
+
+    it('has password input', () => {
+      const loginPage = fixture.nativeElement as HTMLElement;
+      const label = loginPage.querySelector('label[for="password"]');
+      const input = loginPage.querySelector('input[id="password"]');
+      expect(input).toBeTruthy();
+      expect(label).toBeTruthy();
+      expect(label?.textContent).toContain('Password');
+    });
+
+    it('has password type for password input', () => {
+      const loginPage = fixture.nativeElement as HTMLElement;
+      const input = loginPage.querySelector('input[id="password"]') as HTMLInputElement;
+      expect(input?.type).toBe('password');
+    });
+
+    it('has Login button', () => {
+      const loginPage = fixture.nativeElement as HTMLElement;
+      const button = loginPage.querySelector('button');
+      expect(button?.textContent).toContain('Login');
+    });
+
+    it('disables the button initially', () => {
+      const loginPage = fixture.nativeElement as HTMLElement;
+      const button = loginPage.querySelector('button');
+      expect(button?.disabled).toBeTruthy();
+    });
+  });
+
+
+  describe('Interactions', () => {
+
+    let button: any;
+    let httpTestingController: HttpTestingController;
+    let loginPage: HTMLElement;
+
+    const setupForm = async () => {
+      httpTestingController = TestBed.inject(HttpTestingController);
+
+      loginPage = fixture.nativeElement as HTMLElement;
+
+      await fixture.whenStable();
+      const emailInput = loginPage.querySelector('input[id="email"]') as HTMLInputElement;
+      const passwordInput = loginPage.querySelector('input[id="password"]') as HTMLInputElement;
+      emailInput.value = 'user1@mail.com';
+      emailInput.dispatchEvent(new Event('input'));
+      emailInput.dispatchEvent(new Event('blur'));
+      passwordInput.value = 'P4ssword';
+      passwordInput.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      button = loginPage.querySelector('button');
+    }
+
+    it('enables the button when all the fields have vaid input', async () => {
+      await setupForm();
+      expect(button?.disabled).toBeFalsy();
+    });
+
+
+    it('sends email and password to backend after clicking the button', async () => {
+      await setupForm();
+      fixture.detectChanges();
+      button?.click();
+      const req = httpTestingController.expectOne("/api/1.0/auth");
+      const requestBody = req.request.body;
+      expect(requestBody).toEqual({
+        email: 'user1@mail.com',
+        password: 'P4ssword'
+      });
+    });
+
+    it('disabled button when there is an ongoing api call', async () => {
+      await setupForm();
+      button.click();
+      fixture.detectChanges();
+      button.click();
+      httpTestingController.expectOne("/api/1.0/auth");
+      expect(button.disabled).toBeTruthy();
+    })
+
+    it('displays spinner after clicking the submit', async () => {
+      await setupForm();
+      expect(loginPage.querySelector('span[role="status"]')).toBeFalsy();
+      button.click();
+      fixture.detectChanges();
+      expect(loginPage.querySelector('span[role="status"]')).toBeTruthy();
+    })
+
+    xit('displays error after submit failure', async () => {
+      await setupForm();
+      button.click();
+      const req = httpTestingController.expectOne('/api/1.0/auth');
+      req.flush({
+        mesasage: 'Incorrect Credentials'
+      }, {
+        status: 401,
+        statusText: "Unauthorized"
+      });
+      fixture.detectChanges();
+      const error = loginPage.querySelector(`.alert`);
+      expect(error?.textContent).toContain('Incorrect Credentials');
+    });
+
+    it('hides spinner after sign up request fails', async () => {
+      await setupForm();
+      button.click();
+      const req = httpTestingController.expectOne('/api/1.0/auth');
+      req.flush({
+        mesasage: 'Incorrect Credentials'
+      }, {
+        status: 401,
+        statusText: "Unauthorized"
+      });
+      fixture.detectChanges();
+      expect(loginPage.querySelector('span[role="status"]')).toBeFalsy();
+    });
   });
 });
--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../core/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AlertComponent } from '../shared/alert/alert.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, FormsModule, AlertComponent],
   templateUrl: './login.component.html',
   styles: ``
 })
 export class LoginComponent {
 
+  email = '';
+  password = '';
+
+  error = '';
+
+  apiProgress = false;
+
+  constructor(private userService: UserService) { }
+
+  isDisabled() {
+    return !this.email || !this.password;
+  };
+
+  onClickLogin() {
+    this.apiProgress = true;
+    this.userService.autheticate(this.email, this.password).subscribe({
+      next: () => { },
+      error: (err: HttpErrorResponse) => {
+        this.error = err.error.message;
+        this.apiProgress = false;
+      },
+    });
+  }
 }
